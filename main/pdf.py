babcd1f5b87810b4d934f709ebaa6c2fb999b99d
Filip <wareewaa@gmail.com>
2022-06-06 18:32:23 +0200

    polskie znaki w pdfie :D

--- main/pdf.py
+++ main/pdf.py
@@ -1,16 +1,17 @@
 import io
 from reportlab.pdfgen import canvas
 from PIL import Image
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
 
 
 def pdfbuffer(cart):
     # Create a file-like buffer to receive PDF data.
     buffer = io.BytesIO()
-
     # Create the PDF object, using the buffer as its "file."
-
     p = canvas.Canvas(buffer)
-
+    pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
+    p.setFont("Arial", 12)
     attractions_list = list(cart.attractions.all())
     height = 100 + 120 * len(attractions_list)
     p.setPageSize((600,height))
@@ -23,9 +24,6 @@ def pdfbuffer(cart):
         p.drawString(200, y - 30, "Cena: " + str(attraction.price) + "zl")
         p.drawString(200, y - 60, "Szacowany czas: " + str(attraction.time))
         y = y + 120
-
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
 
     # Close the PDF object cleanly, and we're done.
     p.showPage()
